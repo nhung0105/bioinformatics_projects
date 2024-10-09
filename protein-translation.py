cba94aaa2a1fc831f5bd405a4af9b604d4fcb25a
nhung0105 <nnguyen26@wooster.edu>
2024-10-09 16:09:10 -0400

    protein-translation.py

--- protein-translation.py
+++ protein-translation.py
@@ -48,3 +48,51 @@ def translate_dna_to_protein(dna_sequence):
 
 protein_sequence = translate_dna_to_protein(dna_sequence)
 print("Protein Sequence: ", protein_sequence)
+
+
+#Plots
+
+def count_nucleotides(dna_sequence):
+    counts = {
+        "A": dna_sequence.count('A'),
+        "T": dna_sequence.count('T'),
+        "C": dna_sequence.count('C'),
+        "G": dna_sequence.count('G')
+    }
+    return counts
+
+import matplotlib.pyplot as plt
+
+def plot_nucleotide_counts(counts):
+    nucleotides = list(counts.keys()) #retrieve all keys from dictionary
+    values = list(counts.values())
+
+    plt.figure(figsize = (8, 6))
+    plt.bar(nucleotides, values, color=['green', 'blue', 'orange', 'red'])
+    plt.title("Nucleotide Counts")
+    plt. xlabel("Nucleotides")
+    plt.ylabel("Count")
+    plt.show()
+
+# Plotting GC content
+def plot_gc_content(counts):
+    gc_count = counts['G'] + counts['C']
+    at_count = counts['A'] + counts['T']
+    
+    plt.figure(figsize=(8, 6))
+    plt.pie([gc_count, at_count], labels=["GC Content", "AT Content"], autopct='%1.1f%%', colors=['orange', 'blue'])
+    plt.title("GC Content vs AT Content")
+    plt.show()
+
+# Example usage
+filename = "rabbit.fna"
+dna_sequence = read_fasta(filename)
+
+# Count nucleotides
+nucleotide_counts = count_nucleotides(dna_sequence)
+
+# Plot nucleotide counts
+plot_nucleotide_counts(nucleotide_counts)
+
+# Plot GC content
+plot_gc_content(nucleotide_counts)
